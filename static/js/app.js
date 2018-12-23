e0aad6afdc79abf16850c8c13c24e095e360a507
Victor Massé <victor.dhamma@gmail.com>
2018-12-23 21:40:44 +0100

    fix gulp

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,21 @@
 /************************************************************
  HIDE LOADER WHEN FB-COMMENTS RENDERED
 ************************************************************/
-// var targetNode = document.querySelector('.fb-comments');
-// var config = { attributes: true };
-// var i = 0;
-// var callback = function(mutationsList, observer) {
-//   for(var mutation of mutationsList) {
-//     if ( mutation.attributeName == "fb-xfbml-state" ) {
-//       i++;
-//       if ( i == 2 ) {
-//         $("#loader").hide();
-//       }
-//     }
-//   }
-// };
-// var observer = new MutationObserver(callback);
-// observer.observe(targetNode, config);
+var targetNode = document.querySelector('.fb-comments');
+var config = { attributes: true };
+var i = 0;
+var callback = function(mutationsList, observer) {
+  for(var mutation of mutationsList) {
+    if ( mutation.attributeName == "fb-xfbml-state" ) {
+      i++;
+      if ( i == 2 ) {
+        $("#loader").hide();
+      }
+    }
+  }
+};
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
 /************************************************************
  FIX MODAL SCROLLBAR
 ************************************************************/
