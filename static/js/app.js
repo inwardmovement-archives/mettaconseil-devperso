e27d042179be1154625ef915a53e18131bc87886
Victor Massé <victor.dhamma@gmail.com>
2018-12-23 21:31:44 +0100

    fix gulp

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,28 @@
 /************************************************************
  HIDE LOADER WHEN FB-COMMENTS RENDERED
 ************************************************************/
-var targetNode = document.querySelector('.fb-comments');
-var config = { attributes: true };
-var i = 0;
-var callback = function(mutationsList, observer) {
-  for(var mutation of mutationsList) {
-    if ( mutation.attributeName == "fb-xfbml-state" ) {
-      i++;
-      if ( i == 2 ) {
-        $("#loader").hide();
-      }
-    }
-  }
-};
-var observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+// var targetNode = document.querySelector('.fb-comments');
+// var config = { attributes: true };
+// var i = 0;
+// var callback = function(mutationsList, observer) {
+//   for(var mutation of mutationsList) {
+//     if ( mutation.attributeName == "fb-xfbml-state" ) {
+//       i++;
+//       if ( i == 2 ) {
+//         $("#loader").hide();
+//       }
+//     }
+//   }
+// };
+// var observer = new MutationObserver(callback);
+// observer.observe(targetNode, config);
 /************************************************************
  FIX MODAL SCROLLBAR
 ************************************************************/
-// $('#modalDonation')
-//   .on('show.bs.modal', function (e) {
-//     document.querySelector("html").style.overflow = "inherit";
-//   })
-//   .on('hide.bs.modal', function (e) {
-//     document.querySelector("html").style.overflow = "initial";
-//   });
+$('#modalDonation')
+  .on('show.bs.modal', function (e) {
+    document.querySelector("html").style.overflow = "inherit";
+  })
+  .on('hide.bs.modal', function (e) {
+    document.querySelector("html").style.overflow = "initial";
+  });
